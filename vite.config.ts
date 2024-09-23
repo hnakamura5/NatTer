4b6dfcd60d8f7fa21db87dc0793af87080cdb7d2
hnakamura5 <k.nakamura.hirofumi@gmail.com>
2024-09-23 19:14:53 +0900

    moved. but find @ from electron files not resolved yet?

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ console.log(__dirname)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    //tsconfigPaths(),
+    tsconfigPaths(),
     react(),
     electron({
       main: {
